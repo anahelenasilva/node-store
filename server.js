e87f54bd01b517687f051c32fb776482153093d6
Ana Helena <anahelenarp@hotmail.com>
2021-04-28 14:16:35 -0300

    Criando servidor e adicionando rota

--- server.js
+++ server.js
@@ -4,14 +4,24 @@
 
 //FIXME: this is a fixme
 
-import http from "htpp";
-import express from "express";
+const express = require("express");
+const http = require("http");
 const debug = require("debug")("nodestr:server");
 
 const app = express();
 const port = 3000;
-
 app.set("port", port);
 
 const server = http.createServer(app);
 const router = express.Router();
+
+const route = router.get("/", (request, response, next) => {
+  response.status(200).send({
+    title: "Node Store API",
+    version: "0.0.1",
+  });
+});
+
+app.use("/", route);
+
+server.listen(port);
