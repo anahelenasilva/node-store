a075ea5ec5a987304a83dcda10035442827bf51b
Ana Helena <anahelenarp@hotmail.com>
2021-04-29 19:17:21 -0300

    Criando product controller

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,18 +2,10 @@
 
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers/product-controller");
 
-router.post("/", (request, response, next) => {
-  response.status(201).send(request.body);
-});
-
-router.put("/:id", (request, response, next) => {
-  const id = request.params.id;
-  response.status(200).send({ id: id, item: request.body });
-});
-
-router.delete("/", (request, response, next) => {
-  response.status(200).send(request.body);
-});
+router.post("/", controller.post);
+router.put("/:id", controller.put);
+router.delete("/", controller.delete);
 
 module.exports = router;
