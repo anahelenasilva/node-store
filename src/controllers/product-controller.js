17cb465c3bff955995b8deeb938b5e15b4119a79
Ana Helena <anahelenarp@hotmail.com>
2021-04-30 13:19:06 -0300

    Cadastrando e listando os produtos

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,7 +1,32 @@
 "use strict";
 
+const mongoose = require("mongoose");
+const Product = mongoose.model("Product");
+
+exports.get = (request, response, next) => {
+  Product.find({ active: true }, "title price slug")
+    .then((data) => {
+      response.status(200).send(data);
+    })
+    .catch((e) => {
+      response
+        .status(400)
+        .send({ message: "Falha ao cadastrar o produto", data: e });
+    });
+};
+
 exports.post = (request, response, next) => {
-  response.status(201).send(request.body);
+  var product = new Product(request.body);
+  product
+    .save()
+    .then((x) => {
+      response.status(201).send({ message: "Produto cadastro com sucesso" });
+    })
+    .catch((e) => {
+      response
+        .status(400)
+        .send({ message: "Falha ao cadastrar o produto", data: e });
+    });
 };
 
 exports.put = (request, response, next) => {
