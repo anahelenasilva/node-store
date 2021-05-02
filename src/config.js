9fbcee31ed9e23a34c5369f67fd18518121e5059
Ana Helena <anahelenarp@hotmail.com>
2021-05-02 10:52:04 -0300

    Adicionando arquivo de configuração

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = "7a60dff6-edec-406d-8e6b-420daf1ce232";
+global.EMAIL_TEMPLATE = "<strong>{0}</strong>";
+
+module.exports = {
+  connectionString: "connection",
+  sendgridKey: "TDB",
+  containerConnectionString: "TBD",
+};
